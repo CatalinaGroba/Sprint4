3e3fe087c7346fa73d74d39bfff9eb0449e79b5f
SurayMP <suray_pedraza@hotmail.com>
2022-07-03 18:44:12 -0300

    Verificación de Error de Cheques repetidos

--- listado_cheques.py
+++ listado_cheques.py
@@ -2,44 +2,52 @@ import csv
 
 dni = input("Ingrese su DNI: ")
 TipoCheque = input("Ingrese Tipo de los cheques (1:EMITIDO 2:DEPOSITADO ): ")
-estadoCheque = input("Ingrese Estado de los cheques (1:APROBADO 2:PENDIENTE 3:RECHAZADO ): ")
+estadoCheque = input(
+    "Ingrese Estado de los cheques (1:APROBADO 2:PENDIENTE 3:RECHAZADO ): ")
 
-csv_file = csv.reader (open("test.csv", "r"))
-salida = input("Opcion 1: Visualizar en Pantalla - Opcion 2:Descargar archivo csv. Opcion: ")  
-archivo = open (str(dni)+".csv", "w")
+csv_file = csv.reader(open("test.csv", "r"))
+salida = input(
+    "Opcion 1: Visualizar en Pantalla - Opcion 2:Descargar archivo csv. Opcion: ")
+archivo = open(str(dni)+".csv", "w")
 
-if(estadoCheque=='1'):estadoCheque='APROBADO'
-elif(estadoCheque=='2'):estadoCheque='PENDIENTE'
-elif(estadoCheque=='3'):estadoCheque='RECHAZADO'
-else:estadoCheque=''
+if(estadoCheque == '1'):
+    estadoCheque = 'APROBADO'
+elif(estadoCheque == '2'):
+    estadoCheque = 'PENDIENTE'
+elif(estadoCheque == '3'):
+    estadoCheque = 'RECHAZADO'
+else:
+    estadoCheque = ''
 
 
-if(TipoCheque=='1'):TipoCheque='EMITIDO'
-elif(TipoCheque=='2'):TipoCheque='DEPOSITADO'
-else:TipoCheque=''
-
-
-
+if(TipoCheque == '1'):
+    TipoCheque = 'EMITIDO'
+elif(TipoCheque == '2'):
+    TipoCheque = 'DEPOSITADO'
+else:
+    TipoCheque = ''
 
 
 def funcionFiltradoDni():
-    arrayInfo=[]
+    arrayInfo = []
     for row in csv_file:
-        if dni == row [8]:
+        if dni == row[8]:
             arrayInfo.append(row)
     return arrayInfo
+
 
 def filtradoEstadoCheque(DatafiltradoDni):
-    arrayInfo=[]
+    arrayInfo = []
     for row in DatafiltradoDni:
-        if estadoCheque == row [10]:
+        if estadoCheque == row[10]:
             arrayInfo.append(row)
     return arrayInfo
 
+
 def filtradoTipoCheque(DatafiltradoDni):
-    arrayInfo=[]
+    arrayInfo = []
     for row in DatafiltradoDni:
-        if TipoCheque == row [9]:
+        if TipoCheque == row[9]:
             arrayInfo.append(row)
     return arrayInfo
 
@@ -47,34 +55,52 @@ def filtradoTipoCheque(DatafiltradoDni):
 def buscarValoresParaArchivo():
     archivo
     for row in csv_file:
-        if dni == row [8]:
-            archivo.write(row [9] + row [10])
+        if dni == row[8]:
+            archivo.write(row[9] + row[10])
 
 
 def PrintPantalla(data):
     for row in data:
         print('------------')
-        print ("Tipo de cheque: " + row[9] + "\nEstado de cheque: " + row[10]+ '\nDni:' +row[8] )
+        print("Tipo de cheque: " +
+              row[9] + "\nEstado de cheque: " + row[10] + '\nDni:' + row[8])
         print('------------')
-        
 
-def tipoDeSalida():  
+
+def verificarErrorCheque(data):
+    for row in data:
+        coincidencias = 0
+        for row2 in data:
+            if (row[0] == row2[0] and row[3] == row2[3]):
+                coincidencias = coincidencias + 1
+                if coincidencias >= 2:
+                   print("Error: múltiples cheques existentes con el mismo número en esta cuenta")
+                   return True
+
+
+
+def tipoDeSalida():
     if (TipoCheque == ''):
-        return print ('Tipo de cheque no valido')
+        return print('Tipo de cheque no valido')
     if salida == "2":
         buscarValoresParaArchivo()
 
     elif salida == "1":
         data = funcionFiltradoDni()
+        errorRepeticion = verificarErrorCheque(data)
+        if errorRepeticion:
+            return
         data = filtradoTipoCheque(data)
-        if(len(data)>0):
+        if(len(data) > 0):
             # print(data)
-            if(estadoCheque!=''):
+            if(estadoCheque != ''):
                 return PrintPantalla(filtradoEstadoCheque(data))
-        
-        else:print('no hubo coincidencias')
 
-        
+        else:
+            print('no hubo coincidencias')
+
     else:
-        print ("no existe la opcion seleccionada")
+        print("no existe la opcion seleccionada")
+
+
 tipoDeSalida()
