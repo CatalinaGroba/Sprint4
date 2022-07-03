053e1a519f027f7ea6f73a23f5fb24d1b648ea7f
Ignacio Latorre <ignaciolatorreap@gmail.com>
2022-07-03 16:26:20 -0300

    genera el csv correctamente

--- listado_cheques.py
+++ listado_cheques.py
@@ -1,12 +1,12 @@
 import csv
+from datetime import datetime
 
 dni = input("Ingrese su DNI: ")
 TipoCheque = input("Ingrese Tipo de los cheques (1:EMITIDO 2:DEPOSITADO ): ")
 estadoCheque = input("Ingrese Estado de los cheques (1:APROBADO 2:PENDIENTE 3:RECHAZADO ): ")
 
 csv_file = csv.reader (open("test.csv", "r"))
-salida = input("Opcion 1: Visualizar en Pantalla - Opcion 2:Descargar archivo csv. Opcion: ")  
-archivo = open (str(dni)+".csv", "w")
+salida = input("Opcion 1: Visualizar en Pantalla - Opcion 2:Descargar archivo csv. Opcion: ")
 
 if(estadoCheque=='1'):estadoCheque='APROBADO'
 elif(estadoCheque=='2'):estadoCheque='PENDIENTE'
@@ -18,9 +18,10 @@ if(TipoCheque=='1'):TipoCheque='EMITIDO'
 elif(TipoCheque=='2'):TipoCheque='DEPOSITADO'
 else:TipoCheque=''
 
-
-
-
+def getTimestamp(): # el pdf pide esto para el nombre del csv
+    dt = datetime.now();
+    ts = datetime.timestamp(dt);
+    return ts
 
 def funcionFiltradoDni():
     arrayInfo=[]
@@ -43,27 +44,34 @@ def filtradoTipoCheque(DatafiltradoDni):
             arrayInfo.append(row)
     return arrayInfo
 
-
-def buscarValoresParaArchivo():
-    archivo
-    for row in csv_file:
-        if dni == row [8]:
-            archivo.write(row [9] + row [10])
-
-
 def PrintPantalla(data):
     for row in data:
         print('------------')
         print ("Tipo de cheque: " + row[9] + "\nEstado de cheque: " + row[10]+ '\nDni:' +row[8] )
         print('------------')
+
+def printInCsv(data):
+    timestamp = getTimestamp()
+    f = open(str(dni) + "-" + str(timestamp) + ".csv", "a", newline="")
+    writer = csv.writer(f)
+    writer.writerow(("FechaOrigen", "FechaPago", "Valor", "NumeroCuentaDestino"))
+    for row in data:
+        writer.writerow((row[6], row[7], row[5], row[4])) # formato que pide el pdf de la consigna
+    f.close() 
         
 
 def tipoDeSalida():  
     if (TipoCheque == ''):
         return print ('Tipo de cheque no valido')
     if salida == "2":
-        buscarValoresParaArchivo()
-
+        data = funcionFiltradoDni()
+        data = filtradoTipoCheque(data)
+        if(len(data)>0):
+            if(estadoCheque!=''):
+                printInCsv(data)
+                print('CSV generado exitosamente!')
+        else:
+            print('no hubo coincidencias')
     elif salida == "1":
         data = funcionFiltradoDni()
         data = filtradoTipoCheque(data)
@@ -77,4 +85,5 @@ def tipoDeSalida():
         
     else:
         print ("no existe la opcion seleccionada")
+
 tipoDeSalida()
